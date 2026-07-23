7ef85bb5fe0d5eb8faf0152bce3d6b9060c3bbe9
mortex1111 <128226208+mortex1111@users.noreply.github.com>
2026-07-23 04:07:12 +0200

    Spikes
    Il add the drone when I wake up, I made it in another project

---
+++ main/main.gd
@@ -0,0 +1,36 @@
+extends Node2D
+var a = [{"id": 1, "def": 10, "cur": 10, "name": "spike", "num": 0, "spawn": preload("res://Event/Spike/spikeSpawn.tscn")},
+ {"id": 2, "def": 7, "cur": 7, "name": "drone"}]
+var b = []
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("e"):
+		spike()
+		"""var temp: int = 0
+		for i in range(a.size()):
+			temp += a[i].cur
+		temp = randi_range(1, temp)
+		for i in range(a.size()):
+			temp -= a[i].cur
+			if (temp <= 0):
+				passOn(i + 1)
+				break"""
+
+func passOn(id: int):
+	if id == 1:
+		spike()
+	elif id == 2:
+		drone()
+
+func spike():
+	var temp = randi_range(3, 15)
+	for i in range(temp):
+		var ins = a[0].spawn.instantiate()
+		add_child(ins)
+	a[1].num = temp
+
+func drone():
+	print("drone")
